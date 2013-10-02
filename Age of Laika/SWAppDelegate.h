58cd341030fac9dcb88fd413c290742ae3a598b2
Shannon Wynn <shannonwynn@mac.com>
2013-10-01 20:38:45 -0400

    Initial Commit

---
+++ Age of Laika/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  Age of Laika
+//
+//  Created by Shannon Wynn on 10/1/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Age of Laika/SWViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWViewController.h
+//  Age of Laika
+//
+//  Created by Shannon Wynn on 10/1/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWViewController : UIViewController
+
+@end
